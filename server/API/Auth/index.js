9ff6900d808eaa27231f0fff4d0d7927aa6caec4
sanika999 <sanika.dalvi2000@gmail.com>
2021-09-08 23:46:01 +0530

    new repo

---
+++ server/API/Auth/index.js
@@ -0,0 +1,52 @@
+// Library
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Models
+import { UserModel } from "../../database/user";
+
+const Router = express.Router();
+
+/*
+Route    /signup
+Des      Signup with email and password
+Params   none
+Access   Public
+Method   POST
+*/
+Router.post("/signup", async(req, res) => {
+    try {
+        await UserModel.findByEmailAndPhone(req.body.credentials);
+
+        // save to DB
+        const newUser = await UserModel.create(req.body.credentials);
+
+        // generate JWT auth token
+        const token = newUser.generateJwtToken();
+
+        // return
+        return res.status(200).json({ token, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+/*
+Route    /signin
+Des      Signin with email and password
+Params   none
+Access   Public
+Method   POST
+*/
+Router.post("/signin", async(req, res) => {
+    try {
+        const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+        const token = user.generateJwtToken();
+        return res.status(200).json({ token, status: "success" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
+
+export default Router;
