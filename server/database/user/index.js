d7af27339a3a523e9f53e50f203972aaa5081494
sanika999 <sanika.dalvi2000@gmail.com>
2021-09-22 17:47:50 +0530

    add food tab ui

--- server/database/user/index.js
+++ server/database/user/index.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
         email: { type: String, required: true },
         password: { type: String },
         address: [{ detail: { type: String }, for: { type: String } }],
-        PhoneNumber: [{ type: Number }],
+        phoneNumber: [{ type: Number }],
     },
 
     {
